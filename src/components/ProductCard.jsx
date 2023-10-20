0b89e7c71ff1d71f371e7becfebadf434ca07a06
R4ID3NZ4P <iamxtremesifat@gmail.com>
2023-10-20 18:26:02 +0600

    Brand products page finalized

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ data }) => {
@@ -9,9 +10,14 @@ const ProductCard = ({ data }) => {
         brand,
         type,
         price,
-        description,
         rating
     } = data;
+
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current.childNodes[rating-1].setAttribute("checked", true);
+    }, [])
 
     return (
         <div className="card bg-base-100 shadow-xl">
@@ -24,6 +30,17 @@ const ProductCard = ({ data }) => {
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{name}</h2>
+                <h2>Brand: {brand}</h2>
+                <h2>Type: {type}</h2>
+                <h2>Price: {price}$</h2>
+                <h2>Rating:</h2>
+                <div className="rating mb-4" ref={ref}>
+                    <input type="radio" name={`rating${_id}`} className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name={`rating${_id}`} className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name={`rating${_id}`} className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name={`rating${_id}`} className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name={`rating${_id}`} className="mask mask-star-2 bg-orange-400" />
+                </div>
                 <div className="card-actions">
                     <Link to={`/brands/${brand}/${_id}`}><button className="btn bg-cyan-500">Details</button></Link>
                     <Link to={`/brands/${brand}/${_id}/update`}><button className="btn bg-cyan-500">Update</button></Link>
